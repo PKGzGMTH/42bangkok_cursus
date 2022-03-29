64e11c6887895dc3e86b407705f422025b9b9226
PKGzGMTH <peakungg@gmail.com>
2022-03-29 14:09:01 +0700

    rework ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ptippaya <ptippaya@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 21:31:24 by ptippaya          #+#    #+#             */
-/*   Updated: 2022/03/25 23:51:39 by ptippaya         ###   ########.fr       */
+/*   Updated: 2022/03/29 14:04:34 by ptippaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
 	size_t	i;
 
-	i = 0;
-	if (ft_strlen(dst) < n)
-		return (ft_strlen(dst) + ft_strlen(src));
+	i = ft_strlen(dst);
+	if (n == 0)
+		return (ft_strlen(src));
+	else if (n <= i)
+		return (ft_strlen(src) + n);
 	else
 	{
 		while (*src && i < n - 1)
 			dst[i++] = *src++;
-		dst[i] = 0;
-		return (ft_strlen(dst) + n);
+		return (ft_strlen(dst) + ft_strlen(src));
 	}
 }
--- libft/test.c
+++ libft/test.c
@@ -6,7 +6,7 @@
 /*   By: ptippaya <ptippaya@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 22:18:54 by ptippaya          #+#    #+#             */
-/*   Updated: 2022/03/25 23:56:05 by ptippaya         ###   ########.fr       */
+/*   Updated: 2022/03/29 14:06:37 by ptippaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,28 @@
 #include <string.h>
 #include "libft.h"
 
+unsigned int passcount = 0;
+
 void check(int value)
 {
 	static int count = 0;
 
 	count++;
 	if (value)
+	{
 		printf("[%d] \e[38;5;49mPass\e[0;m\n", count);
+		passcount++;
+	}
 	else
 		printf("[%d] \e[38;5;197mError\e[0;m\n", count);
 }
 
-int	main(void)
+void showLeaks()
 {
-	/* test code */
+	;
 }
 
+int	main(void)
+{
+	printf("Total pass: %d\n", passcount);
+}
