7bdf7208e54fda952fd745d2941da7dc69049423
PKGzGMTH <peakungg@gmail.com>
2022-04-06 22:05:50 +0700

    fixed another bug :v

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ptippaya <ptippaya@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 19:45:57 by ptippaya          #+#    #+#             */
-/*   Updated: 2022/03/30 19:50:56 by ptippaya         ###   ########.fr       */
+/*   Updated: 2022/04/06 21:53:39 by ptippaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s)
 	size_t	size;
 
 	size = ft_strlen(s);
-	dst = (char *) malloc (sizeof(char) * (size + 1));
+	dst = (char *) ft_calloc (sizeof(char), size + 1);
 	if (!dst)
 		return (0);
 	ft_memcpy(dst, s, size);
