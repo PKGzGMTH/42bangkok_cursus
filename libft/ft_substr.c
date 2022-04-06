7792593fb4680962234e9c9527ac987d3431111f
PKGzGMTH <peakungg@gmail.com>
2022-04-06 21:36:03 +0700

    fixed bug

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ptippaya <ptippaya@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 19:21:28 by ptippaya          #+#    #+#             */
-/*   Updated: 2022/04/06 17:31:52 by ptippaya         ###   ########.fr       */
+/*   Updated: 2022/04/06 21:27:53 by ptippaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*dest;
 	char	*temp;
 
-	dest = ft_calloc(sizeof(char), 1);
-	dest[0] = 0;
 	if (ft_strlen(s) <= start)
+	{
+		dest = ft_calloc(sizeof(char), 1);
+		ft_bzero(dest, 1);
 		return (dest);
+	}
 	temp = (char *) s + start;
 	if (ft_strlen(temp) < len)
 		len = ft_strlen(temp);
-	dest = (char *) ft_calloc(sizeof(char), len + 1);
+	dest = (char *) ft_calloc(len + 1, sizeof(char));
 	if (!dest)
 		return (0);
 	ft_strlcpy(dest, s + start, len + 1);
