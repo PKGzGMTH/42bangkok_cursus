2e8244cb098554b747b5a76498f218210cdbc449
PKGzGMTH <peakungg@gmail.com>
2022-06-07 20:31:41 +0700

    add some function :v

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ptippaya <ptippaya@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 12:52:20 by ptippaya          #+#    #+#             */
-/*   Updated: 2022/06/04 22:13:22 by ptippaya         ###   ########.fr       */
+/*   Updated: 2022/06/07 20:30:37 by ptippaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@
 # include <stdlib.h>
 # include <stdio.h>
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 200
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
+char	*get_next_rest(char	*rest);
+char	*get_next_dest(char *rest);
+char	*get_next_read(int fd, char *dest);
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strchr(char *s, char c);
 
